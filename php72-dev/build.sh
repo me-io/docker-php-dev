4f1d6a3e3a81b1548661b4e90a58399d6bcdf880
Mohamed Meabed <mo.meabed@gmail.com>
2019-05-08 00:44:18 +0400

    update

--- php72-dev/build.sh
+++ php72-dev/build.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 REPO_NAME="meio/php-dev"
-PHP_VERSION="7.2.9"
+PHP_VERSION="7.2.18"
 BOOT2DOCKER_ID="501"
 BOOT2DOCKER_GID="20"
 DEV_USER="www-data"
