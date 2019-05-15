28492f08dc3678bdbe00d986aa4ae598ddbb967e
Mohamed Meabed <mo.meabed@gmail.com>
2019-05-15 11:37:04 +0400

    update

--- php73-dev/build.sh
+++ php73-dev/build.sh
@@ -35,6 +35,7 @@ if [[ $? != 0 ]]; then
     exit 1
 fi
 
+
 if [[ -z ${TAG_EXIST} || "${FORCE}" == "f" ]]; then
     docker push ${REPO_NAME}:${PHP_VERSION}
     echo "${REPO_NAME}:${DOCKER_TAG} pushed successfully"
